5024ada95ed06b0bf88801cd535b5d6b5c896dfc
Patric <patric@gimgyudong-ui-MacBookPro.local>
2020-06-01 13:15:07 +0900

    refector : characters/info api 수정
    userId 를 받아서 해당 userId 캐릭터 정보를 보내줌

--- controllers/characters/info.js
+++ controllers/characters/info.js
@@ -3,14 +3,16 @@ const { characters } = require("../../models");
 module.exports = {
   post: async (req, res, next) => {
     try {
-      console.log("char ==> ", req.body);
-      if (req.session.userId) {
-        console.log(req.session);
-        const info = await characters.findAll({
-          where: { userId: 1 },
-        });
-        console.log("info ==> ", info);
+      const { userId } = req.body;
+
+      const info = await characters.findAll({
+        where: { userId },
+      });
+
+      if (info[0]) {
         res.status(200).send(info);
+      } else {
+        res.status(404).send({});
       }
     } catch (err) {
       console.log(err);
--- controllers/users/check.js
+++ controllers/users/check.js
@@ -3,7 +3,6 @@ const { users } = require("../../models");
 module.exports = {
   get: async (req, res, next) => {
     try {
-      // res.status(200).send({ success: req.tokenDecoded });
       res.status(200).send({ success: "success" });
     } catch (err) {
       console.log(err);
